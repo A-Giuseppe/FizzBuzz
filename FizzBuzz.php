a0e572591fe0c68708da7944648f27ad76a55d07
Giuseppe <g_amatulli@outlook.com>
2020-10-20 17:38:22 +0200

    refactor - manage multiple of 3 whith a modulo

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -5,7 +5,7 @@ class FizzBuzz
 {
     public function count(int $number){
 
-        if($number === 3 || $number ===6) return "Fizz";
+        if($number % 3 ===  0) return "Fizz";
         if($number === 5) return "Buzz";
 
         return strval($number);

8b966e53d3159a639faf7bc99dafbbd394e315ee
Giuseppe <g_amatulli@outlook.com>
2020-10-20 17:39:50 +0200

    extract iMultiple3

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -5,10 +5,16 @@ class FizzBuzz
 {
     public function count(int $number){
 
-        if($number % 3 ===  0) return "Fizz";
+        if($this->isMultipleOf3($number)) return "Fizz";
         if($number === 5) return "Buzz";
 
         return strval($number);
+    }
+
+
+    public function isMultipleOf3(int $number): bool
+    {
+        return $number % 3 === 0;
     }
 
 }

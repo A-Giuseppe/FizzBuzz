67564425a6c98b3682c9d4713be55b71bcbe1a1c
Giuseppe <g_amatulli@outlook.com>
2020-10-20 17:01:10 +0200

    add - count for input 1 and 2

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,7 +1,13 @@
 <?php
-
+namespace FizzBuzz;
 
 class FizzBuzz
 {
+    public function count($number){
+
+        if($number === 2) return "2";
+
+        return "1";
+    }
 
 }
--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,7 +1,27 @@
 <?php
+require "vendor/autoload.php";
+require "FizzBuzz.php";
 
+use FizzBuzz\FizzBuzz;
+use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
+    public function test_count_given1_shouldReturn1(){
+        //
+        $fizzBuzz = new FizzBuzz();
+        //Act
+        $actual = $fizzBuzz->count(1);
+        //Assert
+        $this->assertEquals("1", $actual);
 
+    }
+
+    public function test_count_given2_shouldReturn2(){
+        $fizzBuzz = new FizzBuzz();
+
+        $actual = $fizzBuzz->count(2);
+
+        $this->assertEquals("2", $actual);
+    }
 }
